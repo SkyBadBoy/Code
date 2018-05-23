5d5caa86db747aaa7137a56c61715bd4ca94b604
MaJian <1039510196@qq.com>
2018-05-23 16:25:22 +0800

    处理跨域问题的session不一致问题

--- src/main/java/com/code/config/cross/Cross.java
+++ src/main/java/com/code/config/cross/Cross.java
@@ -21,11 +21,27 @@ public class Cross extends OncePerRequestFilter {
         response.setCharacterEncoding("utf-8");
         response.setHeader("Content-Type", "application/json");
         //允许所以域名访问，*需要换位制定来源，务必注意这里
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         //允许的访问方式
         response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with,content-type,auth_token");
         response.setHeader("Access-Control-Request-Headers", "x-requested-with,content-type,Accept");
+
+        //以下参数解决由于跨域问题造成的sessIon不一致问题
+        /**
+         *
+         * 前端需设置
+         * axios.defaults.withCredentials=true;
+         *   xhrFields:{
+                withCredentials: true
+             },
+                crossDomain: true
+             })
+         *
+         * */
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("XDomainRequestAllowed","1");
+
         filterChain.doFilter(request, response);
     }
 }
